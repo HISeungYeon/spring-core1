d42037415de72b488e22149636461c206b278869
HISeungYeon <sy.lee5081@gmail.com>
2023-06-30 17:01:46 +0900

    스프링 핵심 기본편 3

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); //memberServiceImpl을 가지고있음
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); //memberServiceImpl을 가지고있음
 //        MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//메서드이름으로 찾음, 반환타입
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
