0ebe16bc9e09fb411c9e46b75147d219eba32371
HISeungYeon <sy.lee5081@gmail.com>
2023-07-03 16:42:00 +0900

    스프링 핵심 기본편 6

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //다 스캔할건데 그 중 뺄 것. 이 어노테이션이 붙은 클래스는 빼줘!
+)
+public class AutoAppConfig {
+
+
+}
